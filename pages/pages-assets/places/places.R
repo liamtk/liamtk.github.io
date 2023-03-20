35937e910039f0a9b40b02b2dca5050027c76c9a
ltk2118 <liam.k@columbia.edu>
2023-03-20 10:32:38 +0800

    add geocodings with photo links

--- pages/pages-assets/places/places.R
+++ pages/pages-assets/places/places.R
@@ -3,340 +3,39 @@ library(leaflet)
 library(htmlwidgets)
 library(tidygeocoder)
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()[["path"]]))
 
-# Australia & NZ ----------------------------------------------------------
+# read in data
+combined <- read_csv("combined.csv")
 
-region_aus_nz <-
-  data.frame("place" = c(
-    "Sydney, NSW",
-    "Perth, WA",
-    "Exmouth, WA",
-    "Gold Coast, QLD",
-    "Melbourne, VIC",
-    "Mount Buller, VIC",
-    "Brisbane, QLD",
-    "Canberra, ACT",
-    "Port Douglas, QLD",
-    "Cairns, QLD",
-    "Hobart, TAS",
-    "Adelaide, SA",
-    "Darwin, NT",
-    "Broome, WA",
-    "Kununurra, WA",
-    "Carnarvon, WA",
-    "Boddington, WA",
-    "Boyup Brook, WA",
-    "Bunbury, WA",
-    "Port Headland, WA",
-    "Albany, WA",
-    "Margaret River, WA",
-    "Kalbarri, WA",
-    "Dalwallinu, WA",
-    "Three Springs, WA",
-    "Geraldton, WA",
-    "Hamilton Island, QLD",
-    "Christchurch, NZ",
-    "Akaroa, NZ",
-    "Lake Pukaki, NZ",
-    "Queenstown, NZ",
-    "Suva, Fiji",
-    "Nadi, Fiji")) %>% 
-  mutate(region = "anz") %>%
+# geocode new (if no new places, ignore -- it will throw an error)
+new_places <- combined %>%
+  filter(is.na(lat) & is.na(long)) %>%
+  select(-lat, -long) %>%
   geocode(address = place, method = "osm")
 
+combined <- combined %>% 
+  filter(!is.na(lat) & !is.na(long)) %>%
+  bind_rows(new_places)
 
-# North Asia -----------------------------------------------------------
+write_csv(combined, "combined.csv")
 
-region_north_asia <-
-  data.frame("place" = c(
-    "Beijing, China",
-    "Shanghai, China",
-    "Hangzhou, Zhejiang",
-    "Guangzhou, Guangdong",
-    "Qiandaohu, Zhejiang",
-    "Huangshan, Anhui",
-    "Xi'an, Shaanxi",
-    "Ordos, Inner Mongolia",
-    "Hongcun, Anhui",
-    "Taipei, Taiwan",
-    "Yehliu, Taiwan",
-    "Sanya, Hainan",
-    "Fuzhou, Fujian",
-    "Ningde, Fujian",
-    "Xiamen, Fujian",
-    "Hong Kong SAR",
-    "Macau SAR",
-    "Urumqi, Xinjiang",
-    "Guilin, Guangxi",
-    "Fenghuang, Hunan",
-    "Zhangjiajie, Hunan",
-    "Dali, Yunnan",
-    "Lijiang, Yunnan",
-    "Shangri-La, Yunnan",
-    "Chongqing, China",
-    "Chengdu, Sichuan",
-    "Leshan, Sichuan",
-    "Hohhot, Inner Mongolia",
-    "Harbin, Heilongjiang",
-    "Jiamusi, Heilongjiang",
-    "Changbaishan, Jilin",
-    "Nanjing, Jiangsu",
-    "Suzhou, Jiangsu",
-    "Tokyo, Japan",
-    "Yokohama, Japan",
-    "Sapporo, Japan",
-    "Osaka, Japan",
-    "Kyoto, Japan",
-    "Seoul, South Korea",
-    "Jeju, South Korea"
-  )) %>% 
-  mutate(region = "noa") %>%
-  geocode(address = place, method = "osm")
-
-
-# Southeast Asia ----------------------------------------------------------
-
-region_southeast_asia <-
-  data.frame("place" = c(
-    "Singapore, Singapore",
-    "Johor Bahru, Malaysia",
-    "Muar, Malaysia",
-    "Ipoh, Malaysia",
-    "New Delhi, India",
-    "Penang, Malaysia",
-    "Jakarta, Indonesia",
-    "Bali, Indonesia",
-    "Kuala Lumpur",
-    "Cameron Highlands, Malaysia",
-    "Malacca, Malaysia",
-    "Kota Kinabalu, Malaysia",
-    "Bangkok, Thailand",
-    "Chiang Mai, Thailand",
-    "Siem Reap, Cambodia",
-    "Phnom Penh, Cambodia",
-    "Saigon, Vietnam",
-    "Hue, Vietnam",
-    "Da Nang, Vietnam",
-    "Hanoi, Vietnam",
-    "Luang Prabang, Laos",
-    "Yangon, Myanmar",
-    "Bagan, Myanmar",
-    "Naypyidaw, Myanmar",
-    "Mawlamyine, Myanmar",
-    "Bago, Myanmar"
-  )) %>% 
-  mutate(region = "sea") %>%
-  geocode(address = place, method = "osm")
-
-
-# Middle East & North Africa ----------------------------------------------
-
-region_mena <-   data.frame("place" = c(
-  "Haifa, Israel",
-  "Ramallah, Palestine",
-  "Hebron, Palestine",
-  "Jerusalem",
-  "Jericho, Palestine",
-  "Acre, Israel"
-)) %>% 
-  mutate(region = "mena") %>%
-  geocode(address = place, method = "osm")
-
-
-# South America -----------------------------------------------------------
-
-region_south_america <-   data.frame("place" = c(
- "Islas Ballestas, Peru",
- "Lima, Peru",
- "Huacachina, Ica, Peru",
- "Cusco, Peru",
- "Machu Picchu, Peru",
- "Bogota, Colombia",
- "Zipaquira, Colombia"
-)) %>% 
-  mutate(region = "sam") %>%
-  geocode(address = place, method = "osm")
-
-# North America -----------------------------------------------------------
-
-region_north_america <- data.frame("place" = c(
-  "New York City, NY",
-  "Greenwood Lake, NY",
-  "Beacon, NY",
-  "New Orleans, LA",
-  "Las Vegas, NV",
-  "San Francisco, CA",
-  "Page, AZ",
-  "Tusayan, AZ",
-  "Grand Canyon North Rim",
-  "Durham, NC",
-  "Cold Spring, NY",
-  "Boston, MA",
-  "Salt Lake City, UT",
-  "Montego Bay, Jamaica",
-  "Yellowstone, WY",
-  "Philadelphia, PA",
-  "Portland, Maine",
-  "Scranton, PA",
-  "Lancaster, PA",
-  "Hoboken, NJ",
-  "Baltimore, MD",
-  "New Haven, CT",
-  "Wilmington, DE",
-  "Toronto, Canada",
-  "Vancouver, Canada",
-  "Washington, D.C.",
-  "Oaxaca, Mexico",
-  "Merida, Mexico",
-  "Tulum, Mexico",
-  "Valladolid, Mexico",
-  "Cancun, Mexico",
-  "Playa Del Carmen, Mexico",
-  "Ciudad De Mexico, Mexico",
-  "Uxmal, Mexico"
-)) %>% 
-  mutate(region = "nam") %>%
-  geocode(address = place, method = "osm")
-
-
-# Europe ------------------------------------------------------------------
-
-region_europe <- data.frame("place" = c(
-  "Paris, France",
-  "Barcelona, Catalunya",
-  "Madrid, Spain",
-  "Berlin, Germany",
-  "Seville, Spain",
-  "Lisbon, Portugal",
-  "Sintra, Portugal",
-  "Porto, Portugal",
-  "Bourg-Saint-Maurice, France",
-  "Cork, Ireland",
-  "Limerick, Ireland",
-  "Galway, Ireland",
-  "Aarhus, Denmark",
-  "Copenhagen, Denmark",
-  "Prague, Czechia",
-  "Milan, Italy",
-  "Catania, Italy",
-  "Palermo, Italy",
-  "Syracuse, Italy",
-  "Agrigento, Italy",
-  "Ragusa, Italy",
-  "Taormina, Italy",
-  "Rome, Italy",
-  "Naples, Italy",
-  "Florence, Italy",
-  "Venice, Italy",
-  "Pisa, Italy",
-  "Lugano, Switzerland",
-  "Vienna, Austria",
-  "Salzburg, Austria",
-  "Melk, Austria",
-  "Bratislava, Slovakia",
-  "Budapest, Hungary",
-  "Hallstatt, Austria",
-  "Cesky Krumlov, Czechia",
-  "Amsterdam, Netherlands",
-  "London, UK",
-  "Oxford, UK",
-  "Cambridge, UK",
-  "Bath, UK",
-  "Bilbao, Basque Country",
-  "Cordoba, Spain",
-  "Donostia, Basque Country"
-)) %>% 
-  mutate(region = "eur") %>%
-  geocode(address = place, method = "osm")
-
-
-# Build data, palette & map -----------------------------------------------
-
-combined <- bind_rows(mget(ls(pattern = "region_")))
-
-saveRDS(combined, file = "../../../files/combined_places.rds")
-
-
-### START HERE TO ADD NEW PLACES
-
-library(tidyverse)
-library(leaflet)
-library(tmap)
-library(htmlwidgets)
-library(tidygeocoder)
-
-combined <- readRDS("../../../files/combined_places.rds")
-
-add_location <- function(df, location_name, region_code){
-  
-  #' @param get_landing_urls a valid region code 
-  #' valid codes: anz  eur  mena nam  noa  sam  sea soa
-  
-  if(region_code %in% c("anz", "eur","mena", "nam", "noa", "sam", "sea", "soa")){
-    
-    new <- data.frame(place = location_name, region = region_code) %>%
-      geocode(address = place, method = "osm")
-    
-    return(unique(rbind(df, new)))
-    
-  }
-  
-  else{
-    print("Invalid region_code.")
-  }
-
-}
-
-# combined <- add_location(combined, "New Delhi, India", "soa")
-
+#init color palette
 pal <- colorFactor(
   palette = c('blue', 'darkorange1', 'black', 'turquoise4', 'red', 'darkorchid4', 'darkgreen'),
   domain = combined$region
 )
 
-
-
+#generate map
 map <- combined %>%
   mutate(region = as.factor(region)) %>% 
   leaflet() %>%
   addCircleMarkers(lng = ~ long, 
                    lat = ~ lat, 
-                   popup = ~place, 
-                   color = ~pal(region), 
-                   stroke = F,
-                   fillOpacity = 0.7,
-                   radius = 4.5) %>%
-  addLayersControl(
-  baseGroups = c("CartoDB.Positron", "OpenStreetMap", "Esri.WorldTopoMap"),
-  options = layersControlOptions(collapsed = FALSE)
-) %>%
-  addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron", 
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
-  addProviderTiles("OpenStreetMap", group = "OpenStreetMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
-  addProviderTiles("Esri.WorldTopoMap", group = "Esri.WorldTopoMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
-  leaflet::fitBounds(lng1 = -90, lat1 = -80, lng2 = 90, lat2 = 80) %>% 
-  leaflet.extras::addFullscreenControl() %>%
-  leaflet.extras::addResetMapButton() %>%
-  leaflet::setView(lat = 0, lng = 0, zoom = 1)
-
-
-map
-
-
-# Add a column 'image_url' to your dataframe with the image URLs
-# combined$image_url <- c("image_url_1", "image_url_2", ...)
-
-combined$image_url <- NA
-combined$image_url[which(combined$place == "Budapest, Hungary")] = "https://i.imgur.com/lak1plE.jpg"
-
-map <- combined %>%
-  mutate(region = as.factor(region)) %>% 
-  leaflet() %>%
-  addCircleMarkers(lng = ~ long, 
-                   lat = ~ lat, 
-                   popup = ~paste(place, "<br><img src='", image_url, "' width='200' height='auto'/>", sep = ""),
+                   popup = ~paste("<b>", place, "</b><br>", 
+                                  "Years visited: ", visited_year, 
+                                  "<br><img src='", image_url, 
+                                  "' width='150' height='auto'/>", sep = ""),
                    color = ~pal(region), 
                    stroke = F,
                    fillOpacity = 0.7,
@@ -346,11 +45,11 @@ map <- combined %>%
     options = layersControlOptions(collapsed = FALSE)
   ) %>%
   addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron", 
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
+                   options = providerTileOptions(minZoom = 1, maxZoom = 7)) %>%
   addProviderTiles("OpenStreetMap", group = "OpenStreetMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
+                   options = providerTileOptions(minZoom = 1, maxZoom = 7)) %>%
   addProviderTiles("Esri.WorldTopoMap", group = "Esri.WorldTopoMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 6)) %>%
+                   options = providerTileOptions(minZoom = 1, maxZoom = 7)) %>%
   leaflet::fitBounds(lng1 = -90, lat1 = -80, lng2 = 90, lat2 = 80) %>% 
   leaflet.extras::addFullscreenControl() %>%
   leaflet.extras::addResetMapButton() %>%
