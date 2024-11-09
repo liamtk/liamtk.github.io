789195edc073461faf4d75beeefd8fc8423a25d6
Liam Tay Kearney <liam.k@columbia.edu>
2024-11-09 17:40:28 -0500

    update places.html map

--- pages/pages-assets/places/places.R
+++ pages/pages-assets/places/places.R
@@ -1,5 +1,6 @@
-# places.R geocoder and travel map with images
+# places.R - Geocoder and Travel Map with Images
 
+# Load necessary libraries
 library(dplyr)
 library(rstudioapi)
 library(readr)
@@ -8,121 +9,143 @@ library(htmlwidgets)
 library(tidygeocoder)
 
 ## INSTRUCTIONS
-## Update the combined.csv file
-## Upload images to imgur and add links if desired
-## Then run this whole script (cmd + shift + enter)
+## Update the combined.csv file with new data.
+## Upload images to Imgur and add links if desired.
+## Run this entire script (Cmd + Shift + Enter).
 
 ## WARNINGS
-## Make sure to close the 'combined.csv' file before executing this script
-## Check the variable "new_places" to see whether the geocode has worked
+## Ensure the 'combined.csv' file is closed before executing this script.
+## Check the variable "new_places" to verify if geocoding was successful.
 
+# Set working directory to the location of this script
 setwd(dirname(getActiveDocumentContext()[["path"]]))
 
-# read in data
+# Read in data from CSV file
 combined <- read_csv("combined.csv")
 
-# geocode new (if no new places, ignore -- it will throw an error)
-if(sum(is.na(combined$lat) | is.na(combined$long)) == 0){
+# Geocode new places if latitude or longitude is missing
+if (sum(is.na(combined$lat) | is.na(combined$long)) == 0) {
   message("LIAM SAYS: There were no new places to geocode")
-  stop("Please try again with actual addresses to geocode...",
-       call. = FALSE)
-  
-} else{
-  
+  stop("Please try again with actual addresses to geocode...", call. = FALSE)
+} else {
   new_places <- combined %>%
     filter(is.na(lat) | is.na(long)) %>%
-    dplyr::select(-lat, -long) %>%
+    select(-lat, -long) %>%
     geocode(address = place, method = "osm")
   
+  # Report successes and failures in geocoding
   successes <- sum(!is.na(new_places$lat) & !is.na(new_places$long))
-  failures <-  sum(is.na(new_places$lat) | is.na(new_places$long))
+  failures <- sum(is.na(new_places$lat) | is.na(new_places$long))
   
-  cat("New places geocoded: \n\n")
-  
+  cat("New places geocoded:\n\n")
   for (i in seq_len(nrow(new_places))) {
-    if (is.na(new_places$lat[i]) | is.na(new_places$long[i])) {
-      cat(paste(new_places$place[i], "[FAILURE]", sep = " "), "\n")
-    } else {
-      cat(paste(new_places$place[i], "[SUCCESS]", sep = " "), "\n")
-    }
+    status <- ifelse(is.na(new_places$lat[i]) | is.na(new_places$long[i]), "[FAILURE]", "[SUCCESS]")
+    cat(paste(new_places$place[i], status), "\n")
   }
   
-  ## Remove input rows and bind newly geocoded rows
-  combined <<- combined %>% 
+  # Update combined data with newly geocoded places
+  combined <<- combined %>%
     filter(!is.na(lat) & !is.na(long)) %>%
     bind_rows(new_places)
   
-  ## Write out the new combined.csv file
+  # Write updated data back to CSV
   message("File combined.csv successfully overwritten")
   write_csv(combined, "combined.csv")
-  
 }
 
-## 2 -- Generate the map 
+# Split data into 'home' and 'away'
+combined_factored <- combined %>%
+  mutate(max_year = sapply(strsplit(visited_year, ","), function(x) max(as.integer(trimws(x)))))
 
-# Create color palette based on the unique continents
-pal <- colorFactor(
-  palette = c('forestgreen', #europe
-              'navyblue', #latin america 
-              'goldenrod4', #middle east & north africa 
-              'firebrick2', #north america 
-              'darkmagenta', #north & east asia 
-              'deepskyblue3', #oceania
-              'darkorange1'), #south and southeast asia
-  domain = combined$region
+home <- combined_factored %>% filter(place %in% c("Perth, WA", "Singapore, Singapore"))
+away <- combined_factored %>% filter(!place %in% c("Perth, WA", "Singapore, Singapore"))
+
+# Create color palettes for the map
+pal <- colorNumeric(
+  palette = viridis::turbo(n = max(combined_factored$max_year, na.rm = TRUE) - 2008),
+  domain = c(2008, max(combined_factored$max_year, na.rm = TRUE)),
+  na.color = "dimgrey"
 )
 
-# make the map
-map <- combined %>%
-  mutate(region = as.factor(region)) %>% 
+reverse_pal <- colorNumeric(
+  palette = viridis::turbo(n = max(combined_factored$max_year, na.rm = TRUE) - 2008),
+  domain = c(-2008, -max(combined_factored$max_year, na.rm = TRUE)),
+  na.color = "dimgrey",
+  reverse = TRUE
+)
+
+# Create the map
+map <- combined_factored %>%
   leaflet() %>%
-  addCircleMarkers(lng = ~ long, 
-                   lat = ~ lat, 
-                   popup = ~paste("<b>", place, "</b><br>", 
-                                  "Years visited: ", visited_year, 
-                                  "<br><img src='", image_url, 
-                                  "' width='150' height='auto'/>", sep = ""),
-                   color = ~pal(region), 
-                   stroke = F,
-                   fillOpacity = 0.7,
-                   radius = 4.5) %>%
+  addMarkers(
+    data = home,
+    lng = ~ long,
+    lat = ~ lat,
+    popup = ~ paste(
+      "<b>", place, "</b><br>",
+      "A place I call home!",
+      "<br><img src='", image_url, "' width='150' height='auto'/>",
+      sep = ""
+    ),
+    icon = leaflet::makeIcon(
+      iconUrl = "https://cdn-icons-png.flaticon.com/512/25/25694.png",
+      iconWidth = 15,
+      iconHeight = 15,
+      iconAnchorX = 12,
+      iconAnchorY = 12,
+      popupAnchorX = 1,
+      popupAnchorY = -12
+    )
+  ) %>%
+  addCircleMarkers(
+    data = away,
+    lng = ~ long,
+    lat = ~ lat,
+    popup = ~ paste(
+      "<b>", place, "</b><br>",
+      "Years visited: ", visited_year,
+      "<br><img src='", image_url, "' width='150' height='auto'/>",
+      sep = ""
+    ),
+    color = ~ pal(max_year),
+    stroke = FALSE,
+    fillOpacity = 0.75,
+    radius = 4.5
+  ) %>%
+  addLegend(
+    pal = reverse_pal,
+    data = away %>% filter(max_year >= 2008 | is.na(max_year)),
+    values = ~ (-max_year),
+    title = "Year",
+    opacity = 1,
+    position = "bottomright",
+    na.label = "<2008",
+    labFormat = labelFormat(big.mark = "", transform = function(x) -x)
+  ) %>%
   addLayersControl(
-    baseGroups = c("CartoDB.Positron","Esri.WorldTopoMap", "OpenStreetMap"),
+    baseGroups = c("CartoDB.Positron", "Esri.WorldTopoMap", "OpenStreetMap"),
     options = layersControlOptions(collapsed = FALSE)
   ) %>%
-  addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron", 
-                   options = providerTileOptions(minZoom = 1, maxZoom = 8)) %>%
-  addProviderTiles("OpenStreetMap", group = "OpenStreetMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 8)) %>%
-  addProviderTiles("Esri.WorldTopoMap", group = "Esri.WorldTopoMap",
-                   options = providerTileOptions(minZoom = 1, maxZoom = 8)) %>%
-  leaflet::fitBounds(lng1 = -90, lat1 = -80, lng2 = 90, lat2 = 80) %>% 
+  addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron", options = providerTileOptions(minZoom = 1, maxZoom = 8)) %>%
+  addProviderTiles("OpenStreetMap", group = "OpenStreetMap", options = providerTileOptions(minZoom = 1, maxZoom = 8)) %>%
+  addProviderTiles("Esri.WorldTopoMap", group = "Esri.WorldTopoMap", options = providerTileOptions(minZoom = 1, maxZoom=8)) %>%
+  leaflet::fitBounds(lng1=-90, lat1=-80, lng2=90, lat2=80) %>%
   leaflet.extras::addFullscreenControl() %>%
   leaflet.extras::addResetMapButton() %>%
-  leaflet::setView(lat = 0, lng = 0, zoom = 1)
+  leaflet::setView(lat=0, lng=0, zoom=1)
 
-# display the map
+# Display the map
 map
 
-# save/write map as widget ----------------------------------------------
+# Save/write map as widget (uncomment to use)
 map %>% saveWidget('places.html') # save as widget
 
 # Git commands
 system("git config --global user.email 'liam.k@columbia.edu'")
-
-# Pull first
-system("git pull")
-
-# Commit message
+system("git pull") # Pull first
 commit_message <- "update places.html map"
-
-# Add changes to the staging area
 system("git add combined.csv")
 system("git add places.html")
-
-# Commit changes with the specified message
+system("git add places.R")
 system2("git", c("commit", "-m", shQuote(commit_message)))
-
-# Push changes to the remote repository
-system("git push -u origin main")
-
+system("git push -u origin main") # Push changes to remote repository
